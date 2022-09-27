4285fd380e8b5d58770e1e7f1ea4ba98ca5e8d2c
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-09-27 16:05:47 -0500

    work through leetcode challenges

---
+++ src/main/java/Solution.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class Solution {
+
+// fizzbuzz practice question
+//    public List<String> fizzBuzz(int n) {
+//        List<String> fizzbuzz = new ArrayList<String>();
+//        for(int i = 1; i <= n; i++){
+//            if(i % 3 == 0 && i % 5 == 0){
+//                fizzbuzz.add("FizzBuzz");
+//            } else if(i % 3 == 0){
+//                fizzbuzz.add("Fizz");
+//            } else if(i % 5 == 0){
+//                fizzbuzz.add("Buzz");
+//            } else{
+//                fizzbuzz.add(String.valueOf(i));
+//            }
+//        }
+//        return fizzbuzz;
+//    }
+
+    public static void main(String[] args) {
+
+        int[] nums = new int[5];
+        nums[0] = 1;
+        nums[1] = 2;
+        nums[2] = 3;
+        nums[3] = 4;
+        nums[4] = 5;
+
+        int[] sumOfNums = new int[5];
+        int sum = 0;
+            for(int num: nums){
+                sum += num;
+                if(num == nums.length-1){
+                    sumOfNums
+                }
+        }
+
+    }
+}
