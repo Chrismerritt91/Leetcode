56fa563f5f4ec578e29b133a39da2ff9b9c9b166
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-10-06 15:02:13 -0500

    work on NodeList focused challenges

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -108,6 +108,114 @@ public class Solution {
 //    }
 //         return false;
 
+//    4) You are given the heads of two sorted linked lists list1 and list2. (possible interview question)
+//
+//Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+//
+//Return the head of the merged linked list.
+
+//    ListNode testNode = new ListNode(0);
+//    ListNode currentNode = testNode;
+//
+//        while(list1 != null && list2 != null){
+//
+//        if(list1.val < list2.val){
+//            currentNode.next = list1;
+//            list1 = list1.next;
+//        }else{
+//            currentNode.next = list2;
+//            list2 = list2.next;
+//        }
+//        currentNode = currentNode.next;
+//    }
+//         if(list1 != null){
+//        currentNode.next = list1;
+//        list1 = list1.next;
+//    }
+//
+//         if(list2 != null){
+//        currentNode.next = list2;
+//        list2 = list2.next;
+//    }
+//
+//         return testNode.next;
+
+//    5) Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+//    ListNode prev = null;
+//
+//        while(head != null){
+//        ListNode nextNode = head.next;
+//        head.next = prev;
+//        prev = head;
+//        head = nextNode;
+//    (head moves the focal point through the list and the prev and head.next switch the direction of the pointer)
+//    }
+//
+//        return prev;
+
+//    6) Given the head of a singly linked list, return the middle node of the linked list.
+//
+//If there are two middle nodes, return the second middle node.
+//    (fast moves twice as fast through the list so when it reaches the end the slow pointer will be at the halfway point)
+
+//    ListNode slow = head;
+//        ListNode fast = head;
+//
+//        while(fast != null && fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//
+//        return slow;
+
+//    *) how to determine if Listnode has a cycle.
+
+//    ListNode slow = head;
+//        ListNode fast = head;
+//
+//        while (slow != fast){
+//
+//            if(fast == null || fast.next == null){
+//                return false
+//            }
+//
+//            slow = slow.next;
+//            fast = fast.next.next;
+//        }
+//        return true;
+
+//    7) Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+//
+//There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+//     public ListNode intersection(ListNode head) {
+//        ListNode slow = head;
+//        ListNode fast = head;
+//        while (fast != null && fast.next != null){
+//            slow = slow.next;
+//            fast = fast.next.next;
+//            if(slow == fast){
+//                return slow;
+//            }
+//        }
+//         return null;
+//    }
+//
+//    public ListNode detectCycle(ListNode head) {
+//        if(head == null || head.next == null){
+//            return null;
+//        }
+//        ListNode intersect = intersection(head);
+//        if(intersect == null) return null;
+//
+//        ListNode start = head;
+//        while(intersect != start){
+//            intersect = intersect.next;
+//            start = start.next;
+//        }
+//        return start;
+//    }
 
     public static void main(String[] args) {
 
