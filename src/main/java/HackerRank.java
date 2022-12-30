2b4e994b95deec6e7a7edadeeb6f3c70ad090392
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2022-12-29 21:09:26 -0600

    complete more exercises on hacker rank to prepare for interview

---
+++ src/main/java/HackerRank.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HackerRank {
+
+    public static void main(String[] args) {
+//        hackerrank assessment questions
+
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(3);
+        arr.add(5);
+        arr.add(7);
+        arr.add(9);
+
+//        1)
+//
+//        double size = arr.size();
+//        double percentage = 1/size;
+//        double positive = 0;
+//        double negative = 0;
+//        double zero = 0;
+//
+//        for(int i = 0; i < arr.size(); i++){
+//            if(arr.get(i) < 0){
+//                negative += percentage;
+//            }else if(arr.get(i) > 0){
+//                positive += percentage;
+//            }else if(arr.get(i) == 0){
+//                zero += percentage;
+//            }
+//        }
+//        System.out.println(String.format("%.6f",positive));
+//        System.out.println(String.format("%.6f",negative));
+//        System.out.println(String.format("%.6f",zero));
+
+//        2)
+//you can also use collections.min or max to get these values
+
+//        Collections.sort(arr);
+//        long max = arr.get(0);
+//        long min = arr.get(arr.size() - 1);
+//        long total = 0;
+//
+//        for(int i = 0; i < arr.size(); i++){
+//            total += arr.get(i);
+//        }
+//
+//        System.out.print(total - max);
+//        System.out.print(" ");
+//        System.out.print(total - min);
+
+//        3)
+
+
+
+        }
+
+    }
+
+
