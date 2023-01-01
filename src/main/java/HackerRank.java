bcb5d6a6d8f0ae00640ebb2c3c76bad594b0c74b
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2022-12-31 20:42:54 -0600

    complete more hackerrank exercises

--- src/main/java/HackerRank.java
+++ src/main/java/HackerRank.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -6,13 +7,16 @@ public class HackerRank {
 
     public static void main(String[] args) {
 //        hackerrank assessment questions
-
+//
         List<Integer> arr = new ArrayList<>();
         arr.add(1);
+        arr.add(1);
+        arr.add(2);
         arr.add(3);
-        arr.add(5);
-        arr.add(7);
-        arr.add(9);
+        arr.add(3);
+        arr.add(4);
+        arr.add(4);
+
 
 //        1)
 //
@@ -52,8 +56,48 @@ public class HackerRank {
 //        System.out.print(total - min);
 
 //        3)
+//        convert any given time to military time
+//        String s = "04:59:59AM";
+//        String[] stringArr = s.split(":");
+//
+//        String hours = stringArr[0];
+//        String minutes = stringArr[1];
+//        String seconds = stringArr[2];
+//
+//        if(seconds.contains("PM") && Integer.parseInt(hours) < 12){
+//            hours = String.valueOf(Integer.parseInt(hours) + 12);
+//        }
+//        if(seconds.contains("AM") && hours.equals("12")){
+//            hours = "00";
+//        }
+//
+//        String militaryTime = hours + ":" + minutes + ":" + seconds.substring(0,2);
+//
+//        System.out.println(militaryTime);
 
+//        4)
+//        find the median of an array
 
+//        Collections.sort(arr);
+//        int n = arr.size();
+//
+//        for(int i = 0, j = n-1; i < n && j > 0; i++, j--){
+//            if(i == j){
+//                System.out.println(i);
+//            }
+//        }
+
+//        5)
+//not finished
+//        Collections.sort(a);
+//        if(a.size() <= 1){
+//            return a.get(0);
+//        }
+//        for(int i = 1; i < a.size(); i++){
+//         if(a.get(i) != a.get(i - 1) && a.get(i) != a.get(i + 1)){
+//             return a.get(i);
+//         }
+//        }
 
         }
 
