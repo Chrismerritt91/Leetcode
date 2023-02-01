58a1ad15a6504ceaab8110cdbdb627e6d4ff7d96
Chris Merritt <102829558+Chrismerritt91@users.noreply.github.com>
2023-02-01 15:30:57 -0600

    complete more challenges, work harder to find more simple solutions

--- src/main/java/HackerRank.java
+++ src/main/java/HackerRank.java
@@ -742,10 +742,54 @@ public class HackerRank {
 
 //        -----------------------------------------------------------------------------------------
 
-//        26) Drawing book/
+//        26) Drawing book/ A teacher asks the class to open their books to a page number. A student can either start turning pages from the front of the book or from the back of the book. They always turn pages one at a time. When they open the book, page 1 is always on the right side:
+//        When they flip page 1, they see pages 2 and 3. Each page except the last page will always be printed on both sides. The last page may only be printed on the front, given the length of the book. If the book is n pages long, and a student wants to turn to page p, what is the minimum number of pages to turn? They can start at the beginning or the end of the book.
+//        Given n and p, find and print the minimum number of pages that must be turned in order to arrive at page p.
 
+//        int frontCount = p/2;
+//        int rearCount = (n-p)/2;
+//
+//        if(n%2==0 && p%2!=0){
+//            rearCount++;
+//        }
+//
+//        if(frontCount > rearCount){
+//            return rearCount;
+//        }else{
+//            return frontCount;
+//        }
 
+//        ----------------------------------------------------------------------------------------
 
+//        27) MAX MIN/ You will be given a list of integers, arr, and a single integer k. You must create an array of length k from elements of arr such that its unfairness is minimized. Call that array sub. Unfairness of an array is calculated as arr[MAX] - arr[MIN]
+//        Function Description
+//        Complete the maxMin function in the editor below.
+//        maxMin has the following parameter(s):
+//        int k: the number of elements to select
+//        int arr[n]:: an array of integers
+//        Returns
+//        int: the minimum possible unfairness.
+
+//        Collections.sort(arr);
+//        List<Integer> sub = new ArrayList<Integer>();
+//        int result = 0;
+//        for(int i = 0; i<arr.size(); i++){
+//            if((i+k) <= arr.size()){
+//                sub = arr.subList(i, i+k);
+//                int max = sub.get(k-1);
+//                int min = sub.get(0);
+//                int difference = max - min;
+//                if(result == 0)result = difference;
+//                if(difference < result){
+//                    result = difference;
+//                }
+//            }
+//        }
+//        return result;
+
+//        ----------------------------------------------------------------------------------------
+
+//        28)
 
 
 
